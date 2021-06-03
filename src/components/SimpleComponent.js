435ba748d68b45c915c7890a9235ebfd88c9d283
lisellelogan <liselle.logan@yahoo.com>
2021-06-03 12:50:46 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,32 @@
-// Code SimpleComponent Here
+// In the components/SimpleComponent.js file, create a SimpleComponent component.
+
+// The component should be declared as a class (instance of Component) so that it can carry state.
+
+// The component should have a state property called mood that has a default value of happy.
+
+// The component should simply render its current mood state to the page in a div.
+
+// The component should implement a handleClick function that can serve as a callback to the <div>'s click event. When clicked, the component's mood should toggle between happy and sad states.
+
+import React, { Component } from 'react';
+
+export default class SimpleComponent extends Component {
+
+    state = {
+        mood: 'happy'
+    }
+
+    handleClick = () => {
+        this.setState({
+            mood: 'sad'
+        })
+    }
+
+    render(){
+        return(
+            <div onClick={this.handleClick}>
+                {this.state.mood}
+            </div>
+        )
+    }
+}
